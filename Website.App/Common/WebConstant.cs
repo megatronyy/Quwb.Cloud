265534171080696abe280e315f359656c180e166
屈文斌 <twfx7758@hotmail.com>
2017-12-01 13:51:21 +0800

    common

---
+++ Website.App/Common/WebConstant.cs
@@ -0,0 +1,16 @@
+﻿using Lib.Framework.Core.IoC;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Website.App.Common
+{
+    public sealed class WebConstant
+    {
+        private static IConfigurationSection _section = IoCContainer.Resolve<IConfiguration>().GetSection("WebConstant");
+
+        public static string WEBNAME = _section.GetValue<string>("WebName");
+    }
+}
