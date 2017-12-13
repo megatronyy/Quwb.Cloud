10763a00cf8b4d32fe6e489d0406370918fd21f1
屈文斌 <twfx7758@hotmail.com>
2017-12-13 14:10:34 +0800

    换位置

---
+++ Services/Distribution.Services/Common/WebConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Distribution.Services.Common
+{
+    public class WebConfig
+    {
+        public static string ShopHouseRW
+        {
+            get { return GetConnectionStrings("ShopHouseRW"); }
+        }
+
+        public static string GetConnectionStrings(string key)
+        {
+            string reValue = "";
+            if (ConfigurationManager.ConnectionStrings[key] != null)
+            {
+                reValue = ConfigurationManager.ConnectionStrings[key].ToString();
+            }
+            return reValue;
+        }
+    }
+}
